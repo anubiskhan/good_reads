1b20fa9c99d337bab92209300c7f6d6b29d22182
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-30 18:33:10 -0600

    Adds book model test and validations in book model

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,7 +1,9 @@
+require 'rails_helper'
+
 describe Book do
   describe 'validations' do
     context 'validates book attributes' do
-
+      it {should validate_presence_of(:title)}
     end
   end
 end
